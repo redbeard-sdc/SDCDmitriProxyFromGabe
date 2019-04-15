a30b606edbc6fc68c107e1d113fa17825e6d6407
gaborszekely <gabor_szekely@hotmail.com>
2019-04-14 18:59:58 -0600

    Proxy working, properly styled homepage

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require("path");
+const cors = require("cors");
+const httpProxy = require("http-proxy");
+
+const proxy = httpProxy.createProxyServer({});
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.all("/api/*", (req, res) => {
+  proxy.web(req, res, {
+    target: "http://ec2-18-222-0-232.us-east-2.compute.amazonaws.com/"
+  });
+});
+
+app.all("/locations/*", (req, res) => {});
+
+app.all("/prices/*", (req, res) => {});
+
+app.all("/hotels/*", (req, res) => {
+  proxy.web(req, res, {
+    target: "http://ec2-18-188-161-215.us-east-2.compute.amazonaws.com/"
+  });
+});
+
+app.all("/header/*", (req, res) => {
+  proxy.web(req, res, {
+    target: "http://ec2-3-17-179-159.us-east-2.compute.amazonaws.com/"
+  });
+});
+
+app.listen(port, () => console.log(`Proxy server running on port ${port}`));
