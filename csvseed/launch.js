d60663db7c34c95cb5506979d8bfc17758ff1886
dmta9796 <dmta9796@colorado.edu>
2019-04-20 17:26:08 -0600

    change to launch script to reduce parallel heap calls.

--- csvseed/launch.js
+++ csvseed/launch.js
@@ -4,17 +4,23 @@ const {seedHotels} = require("./HotelsCSV");
 const {seedUsers} = require("./UsersCSV");
 
 
-const limit = 1000;
-batchsize = 10;
+const limit = 10000000;
+batchsize = 10000;
 
 const promiseHotels = seedHotels(limit,batchsize);
-Promise.all(promiseHotels).then(() => {console.log("done seeding hotels")});
-
-const promiseUsers = seedUsers(limit,batchsize);
-Promise.all(promiseUsers).then(() => {console.log("done seeding users")});
-
-const promiseReviews = seedReviews(limit,batchsize);
-Promise.all(promiseReviews).then(() => {console.log("done seeding reviews")});
-
-const promiseAnswers = seedAnswers(limit,batchsize);
-Promise.all(promiseAnswers).then(() => {console.log("done seeding answers")});
+Promise.all(promiseHotels).then(() => {
+    console.log("done seeding hotels");
+    const promiseUsers = seedUsers(limit,batchsize);
+    Promise.all(promiseUsers).then(() => {
+        console.log("done seeding users");
+        const promiseReviews = seedReviews(limit,batchsize);
+        Promise.all(promiseReviews).then(() => {
+            console.log("done seeding reviews");
+            const promiseAnswers = seedAnswers(limit,batchsize);
+            Promise.all(promiseAnswers).then(() => {
+                console.log("done seeding answers");
+                console.log("done seeding with everything");
+            });
+        });
+    });
+});
